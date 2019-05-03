de7e5cf47894f77b0e20d70b16da97a93f37e677
Pratama Setya Aji <pratamasetya99@gmail.com>
2019-05-03 23:07:53 +0700

    trial override back button

--- lib/example/navigation/Settings.dart
+++ lib/example/navigation/Settings.dart
@@ -4,16 +4,40 @@ import 'package:flutter/material.dart';
 class Settings extends StatelessWidget {
   static const routeName = '/settingArguments';
 
+  Future<bool> _onWillPop(BuildContext context) {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+                title: new Text('Are you sure?'),
+                content: new Text('Do you want to exit an App'),
+                actions: <Widget>[
+                  new FlatButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: new Text('No'),
+                  ),
+                  new FlatButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: new Text('Yes'),
+                  ),
+                ],
+              ),
+        ) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final SettingsArgument settingsArgument =
         ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Text(settingsArgument.lang),
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: Center(
+          child: Text(settingsArgument.lang),
+        ),
       ),
     );
   }
